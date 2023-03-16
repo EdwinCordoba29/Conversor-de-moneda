3cbb921535e55795144abf9f8d72fc5431a09060
Edwin <reifercordoba@hotmail.com>
2023-03-16 13:16:07 -0500

    Se crea el menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.JOptionPane;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Menu menu = new Menu();
+        String opcion = menu.menuPrincipal();
+        JOptionPane.showMessageDialog(null, "Usted seleccionó " + opcion, "Resultado",JOptionPane.INFORMATION_MESSAGE);
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Menu {
+    public String menuPrincipal(){
+        Object[] opciones = {"Conversor de Moneda","Conversor de Temperatura"};
+        String opcion = JOptionPane.showInputDialog(null, "Seleccione una opción: ","Menu",JOptionPane.PLAIN_MESSAGE,null,opciones,opciones[0]).toString();
+        return opcion;
+    }
+}
